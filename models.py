8394fe4c1657bc3f3633d55171e0c3d9e3e2cad9
m.koohestani <angelmoonstar77@gmail.com>
2024-02-23 21:33:03 +0330

    Remove models

--- models.py
+++
@@ -1,7 +0,0 @@
-class Email:
-    def __init__(self, subject, receivers, html_msg, filepath=None, filename=None):
-        self.subject = subject
-        self.receivers = receivers
-        self.html_msg = html_msg
-        self.filepath = filepath
-        self.filename = filename
